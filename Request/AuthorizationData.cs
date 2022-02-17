d9152eb9ed5dc12a1f219803217996bc4c39b85f
Tuoicm18 <66233758+Tuoicm18@users.noreply.github.com>
2022-02-17 14:52:48 +0700

    Update for format json

--- Request/AuthorizationData.cs
+++ Request/AuthorizationData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,6 @@ using System.Threading.Tasks;
 
 namespace PluginICAOClientSDK.Request {
     public class AuthorizationData {
-        public List<AuthorizationElement> authContentList { get; set; }
-        public List<AuthorizationElement> multipleSelectList { get; set; }
-        public List<AuthorizationElement> singleSelectList { get; set; }
-        public List<AuthorizationElement> nameValuePairList { get; set; }
+        public PrepareAuthData authorizationData { get; set; }
     }
 }
