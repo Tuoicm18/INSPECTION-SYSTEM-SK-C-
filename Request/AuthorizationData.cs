12a623c4fa6af46aa38b1bc03ee97ba81ff097ad
Tuoicm18 <66233758+Tuoicm18@users.noreply.github.com>
2022-02-18 10:42:30 +0700

    Fix json AuthorizationData property

--- Request/AuthorizationData.cs
+++ Request/AuthorizationData.cs
@@ -7,6 +7,16 @@ using System.Threading.Tasks;
 
 namespace PluginICAOClientSDK.Request {
     public class AuthorizationData {
-        public PrepareAuthData authorizationData { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public List<AuthorizationElement> authContentList { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public List<AuthorizationElement> multipleSelectList { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public List<AuthorizationElement> singleSelectList { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public List<AuthorizationElement> nameValuePairList { get; set; }
     }
 }
