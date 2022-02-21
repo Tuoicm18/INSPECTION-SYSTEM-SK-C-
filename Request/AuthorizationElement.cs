c4152076dee4e30f693cf0ee14cebbbaf19edfa3
Tuoicm18 <66233758+Tuoicm18@users.noreply.github.com>
2022-02-21 09:55:27 +0700

    Update AuthElementType for render Layout result authorization data

--- Request/AuthorizationElement.cs
+++ Request/AuthorizationElement.cs
@@ -1,12 +1,17 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PluginICAOClientSDK.Request {
     public class AuthorizationElement {
+        [JsonIgnore]
+        public AuthElementType type { get; set; }
+
         public int ordinary { get; set; }
 
         [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
@@ -22,6 +27,25 @@ namespace PluginICAOClientSDK.Request {
         public Dictionary<string, bool> singleSelect { get; set; }
 
         [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
-        public Dictionary<string, bool> nameValuePair { get; set; }
+        public Dictionary<string, string> nameValuePair { get; set; }
+    }
+
+    public enum AuthElementType {
+        [Description("Content")]
+        Content,
+        [Description("Multiple")]
+        Multiple,
+        [Description("Single")]
+        Single,
+        [Description("NVP")]
+        NVP
+    }
+
+    internal static class Extensions {
+        public static string ToDescription(this Enum value) {
+            FieldInfo field = value.GetType().GetField(value.ToString());
+            DescriptionAttribute attribute = Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) as DescriptionAttribute;
+            return attribute == null ? value.ToString() : attribute.Description;
+        }
     }
 }
