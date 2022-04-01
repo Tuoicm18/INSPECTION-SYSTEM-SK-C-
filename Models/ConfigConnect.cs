98422e40bfdf1ab6028c762e014c023c910ee1c2
Tuoicm18 <66233758+Tuoicm18@users.noreply.github.com>
2022-04-01 10:53:53 +0700

    Update Field  json ConfigConnect

--- Models/ConfigConnect.cs
+++ Models/ConfigConnect.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 namespace PluginICAOClientSDK.Models {
     public class ConfigConnect {
+        public bool automaticEnabled { get; set; }
         public bool mrzEnabled { get; set; }
         public bool imageEnabled { get; set; }
         public bool dataGroupEnabled { get; set; }
