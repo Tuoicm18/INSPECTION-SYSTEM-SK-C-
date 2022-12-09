f58b5f09aa5ea742b0ef33cd401612ee19aeaeb5
Tuoicm18 <66233758+Tuoicm18@users.noreply.github.com>
2022-12-09 08:48:54 +0700

    Updatge AssemblyInfo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PluginICAOClientSDK")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("CheckID Client SDK")]
+[assembly: AssemblyDescription("SDK used for CheckID Client Plugin")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PluginICAOClientSDK")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("MOBILE-ID TECHNOLOGIES AND SERVICES JOINT STOCK COMPANY")]
+[assembly: AssemblyProduct("CheckID Client SDK")]
+[assembly: AssemblyCopyright("Copyright © 2022 MOBILE-ID TECHNOLOGIES AND SERVICES JOINT STOCK COMPANY")]
+[assembly: AssemblyTrademark("Secure and Convenience Identity")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.22.11.2")]
+[assembly: AssemblyFileVersion("1.22.11.2")]
